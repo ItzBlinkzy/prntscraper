d56bed5296204a05d53042c05ef40011e03b2afa
unknown <kitey13579@gmail.com>
2021-01-27 20:39:01 +0000

    Format changes and better looking input

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
 import requests
 import threading
 from bs4 import BeautifulSoup
-import string
+from multiprocessing import Process
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
-ascii_lowercase = string.ascii_lowercase
+
 
 class ImageScraper:
     def __init__(self, two_chars, point_from, point_max):
@@ -12,42 +12,55 @@ class ImageScraper:
         self.point_from = point_from
         self.point_max = point_max
 
-    def download_image(self, filename, link): 
-        html_req = requests.get(link, headers={"User-Agent": user_agent}, allow_redirects=False) # user agent because cloudflare security is gay
+    def download_image(self, filename, link):
+        html_req = requests.get(link, headers={"User-Agent": user_agent}, allow_redirects=False)  # user agent because cloudflare security is gay
+
         if html_req.status_code == 302:
-            print('\x1b[0;30;41m' + 'Link does not exist' + '\x1b[0m'+"\n to fix please follow the instructions in the github repo. [link it here bai]")
+            print("\x1b[0;30;41m" "Link does not exist" "\x1b[0m"". To fix please follow the instructions in the github repo.")
             return
         soup = BeautifulSoup(html_req.content, "html.parser")
         images = soup.find_all("img", {"class": "no-click screenshot-image"})
-        
+
         for tag in images:
             src_img_from_html = tag["src"]
             if not src_img_from_html.startswith("http"):
-                print('\x1b[0;33;40m' + 'Link is invalid or removed' + '\x1b[0m', link)
+                print("\x1b[0;36;40m" "Screenshot is removed." "\x1b[0m", link)
                 return
             img = requests.get(src_img_from_html)
-        
+
             with open(f"./images/{filename}.png", "wb") as file:
                 file.write(img.content)
                 file.close()
                 print(f"Downloaded {link}")
         return
-    
-    
+
     def get_all_images(self):
+        if not self.two_chars.isalpha():
+            raise Exception("\x1b[0;31;40m" "Alphabetic characters only." "\x1b[0m")
+
         if len(self.two_chars) != 2:
-           raise Exception('\x1b[0;31;40m' + f'Expected (2) characters got {len(self.two_chars)}' + '\x1b[0m')
-       
-        elif not self.two_chars.lower().isalpha():
-            raise Exception('\x1b[0;31;40m' + 'Alphabetic characters only.' + '\x1b[0m')
-        
+            raise Exception("\x1b[0;31;40m" f"Expected (2) characters got {len(self.two_chars)}" "\x1b[0m")
+
+        if self.point_from >= self.point_max:
+            raise Exception("\x1b[0;31;40m" "Start point must be lower than end." "\x1b[0m")
+
         while self.point_from <= self.point_max:
             octal_version = str(self.point_from).zfill(4)
             img_link = f"https://prnt.sc/{self.two_chars}{octal_version}"
             self.download_image(img_link[15:], img_link)
             self.point_from += 1
-        print('\x1b[0;32;40m' + 'Completed, look at ./images folder.' + '\x1b[0m')
+
+        print("\x1b[0;32;40m" "Completed, look at ./images folder." "\x1b[0m")
 
 
-m1 = ImageScraper("tq", 250, 400).get_all_images()
-#make multiple instances and then use threading for both of them simulataneously
+if __name__ == "__main__":
+    try:
+        chars = input("\x1b[0;33;40m" "Enter any two random letters in the alphabet. Example [gy] ""\x1b[0m")
+        start_point = int(input("\x1b[0;33;40m" "Enter a Point below 9999 to start from: " "\x1b[0m"))
+        end_point = int(input("\x1b[0;33;40m" "Enter an End Point below 9999 to end: " "\x1b[0m"))
+
+        m1 = ImageScraper(chars, start_point, end_point).get_all_images()
+    except ValueError:
+        raise Exception("\x1b[0;31;40m" "Start and End points must be integers." "\x1b[0m")
+
+# make multiple class instances and then use threading for both of them simulataneously
