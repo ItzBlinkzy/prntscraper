f6ec97b26a94a450c0ace02186583588868a6f82
unknown <kitey13579@gmail.com>
2021-01-29 21:18:06 +0000

    v2.0.0 - Changed functionality of how the class is called it no longer takes any parameters

--- scraper.py
+++ scraper.py
@@ -36,64 +36,73 @@ class ImageScraper:
         return True
     
     
-    def get_random_images(self, limit):
-        count = 0
+    
+    def get_random_images(self):
+        limit = input("\x1b[0;33;40m" " How many images would you like?: " "\x1b[0m")
+        if not limit.isdigit():
+            raise Exception("\x1b[0;31;40m" "You must enter an integer for the limit" "\x1b[0m")
+        
+        limit = int(limit)
+        
+        img_count = 0
         if limit < 1:
-            print("\x1b[0;31;40m" "Below minimum limit (1)" "\x1b[0m")
-            return
+            return "\x1b[0;31;40m" "Below minimum limit (1)" "\x1b[0m"
         alphanum = string.ascii_lowercase + "1234567890"
         
-        while count < limit:
+        while img_count < limit:
             randchars = "".join([alphanum[alphanum.index(random.choice(alphanum))] for i in range(6)])
             img_link = f"https://prnt.sc/{randchars}"
             downloaded = self.download_image(randchars, img_link)
             
             if downloaded:
-                count += 1
-            print(count)
-        print("\x1b[0;32;40m" "Completed, look at ./images folder." "\x1b[0m")
+                img_count += 1
+            print(img_count)
+        return "\x1b[0;32;40m" f"Successfully downloaded {img_count} images. Look at ./images folder."  "\x1b[0m"
 
 
-    def get_input_images(self, two_chars, point_from, point_max):
-        if not two_chars.isalpha():
-            raise Exception("\x1b[0;31;40m" "Alphabetic characters only." "\x1b[0m")
 
-        if len(two_chars) != 2:
-            raise Exception("\x1b[0;31;40m" f"Expected (2) characters got {len(two_chars)}" "\x1b[0m")
+    def get_input_images(self):
+        try:
+            img_count = 0
+            two_chars = input("\x1b[0;33;40m" "Enter any two random letters in the alphabet. Example [gy]: " "\x1b[0m").lower()
+            point_from = int(input("\x1b[0;33;40m" "Enter a Point below 9999 to start from: " "\x1b[0m"))
+            point_max = int(input("\x1b[0;33;40m" "Enter an End Point below 9999 to end: " "\x1b[0m"))
 
-        if point_from >= point_max:
-            raise Exception("\x1b[0;31;40m" "Start point must be lower than end." "\x1b[0m")
+            if not two_chars.isalpha():
+                return "\x1b[0;31;40m" "Alphabetic characters only." "\x1b[0m"
 
-        while point_from <= point_max:
-            octal_version = str(point_from).zfill(4)
-            img_link = f"https://prnt.sc/{two_chars}{octal_version}"
-            downloaded = self.download_image(img_link[15:], img_link)
-            if downloaded:
+            if len(two_chars) != 2:
+                return "\x1b[0;31;40m" f"Expected (2) characters got {len(two_chars)}" "\x1b[0m"
+
+            if point_from >= point_max:
+                return "\x1b[0;31;40m" "Start point must be lower than end." "\x1b[0m"
+
+            while point_from <= point_max:
+                octal_version = str(point_from).zfill(4)
+                img_link = f"https://prnt.sc/{two_chars}{octal_version}"
+                downloaded = self.download_image(img_link[15:], img_link)
+                if downloaded:
+                    img_count += 1
                 point_from += 1
-
-        print("\x1b[0;32;40m" "Completed, look at ./images folder." "\x1b[0m")
+            return "\x1b[0;32;40m" f"Successfully downloaded {img_count} images. Look at ./images folder."  "\x1b[0m"
+        
+        except ValueError:
+            return "\x1b[0;31;40m" "Start and End points must be integers." "\x1b[0m"
 
 
 if __name__ == "__main__":
     choose_input = input("\x1b[0;33;40m" " Would you like to customize your input [Input]. [Random] for random: " "\x1b[0m").lower()
+    
     if choose_input == "random":
-        try:
-            limit = int(input("\x1b[0;33;40m" " How many images would you like?: " "\x1b[0m"))
-            m1 = ImageScraper().get_random_images(limit)
-            
-        except ValueError:
-            raise Exception("\x1b[0;31;40m" "Limit must be integers." "\x1b[0m")
+        m1 = ImageScraper().get_random_images()
+        print(m1)
     
     elif choose_input == "input":
-        try:
-            chars = input("\x1b[0;33;40m" "Enter any two random letters in the alphabet. Example [gy]: " "\x1b[0m").lower()
-            start_point = int(input("\x1b[0;33;40m" "Enter a Point below 9999 to start from: " "\x1b[0m"))
-            end_point = int(input("\x1b[0;33;40m" "Enter an End Point below 9999 to end: " "\x1b[0m"))
-
-            m2 = ImageScraper().get_input_images(chars, start_point, end_point)
-        except ValueError:
-            raise Exception("\x1b[0;31;40m" "Start and End points must be integers." "\x1b[0m")
+        m2 = ImageScraper().get_input_images()
+        print(m2)
+        
     else:
         print("You did not enter 'input' or 'random'")
 
 # make multiple class instances and then use threading for both of them simulataneously
+#put all \x1b[0;33;40m and \x1b[0m in vars or use a color moduleq
