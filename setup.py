adf651abebf4c52460902b574c96dd43033267ea
unknown <kitey13579@gmail.com>
2021-01-31 01:57:25 +0000

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="prnt.sc-scraper",
-    version="2.1.1",
+    version="2.1.2",
     description="Pulls random images from https://prnt.sc",
     long_description=long_description,
     long_description_content_type="text/markdown",
