bedee143cb64d5a3157bfca9b09c546427d7bb31
unknown <kitey13579@gmail.com>
2021-01-31 00:44:10 +0000

    bug fix

--- setup..py
+++ setup..py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -20,6 +20,6 @@ setup(
     ],
     keywords="scraper - prnt.sc",
     python_requires=">=3.7",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     install_requires=["requests", "bs4"],
 )
